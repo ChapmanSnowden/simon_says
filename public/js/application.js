b4a6ab9a22111b13bfd0d327bce2b5f51dbd1629
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-08-19 22:24:10 -0700

    fixed typos in app.js and added json to index

--- public/js/application.js
+++ public/js/application.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
-  $('#get_color').on('click', function(evt){
-    evt.preventDefault();
+  $('#get_color').on('click', function(e){
+    e.preventDefault();
     var request = $.ajax({
-      url: "/color"
+      url: "/color",
       type: "post"
-    }):
-    request.done(function)(response){
-      var json_response = $.parseJSON(response)
-      console.log(json_response)
-      $("ul li:nth-child("+json_response+")").css('background-color',json_response.color)
+    });
+
+    request.done(function(response){
+      var json_response = $.parseJSON(response);
+      console.log(json_response);
+      $("ul li:nth-child("+json_response.cell+")").css('background-color', json_response.color);
     });
   });
 });

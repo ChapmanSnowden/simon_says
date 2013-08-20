d86916f817cdb82c520b7e3ceea9ca10c60a55fa
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-08-19 21:44:36 -0700

    framework for application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-
+  $('#get_color').on('click', function(evt){
+    evt.preventDefault();
+    var request = $.ajax({
+      url: "/color"
+      type: "post"
+    }):
+    request.done(function)(response){
+      var json_response = $.parseJSON(response)
+      console.log(json_response)
+      $("ul li:nth-child("+json_response+")").css('background-color',json_response.color)
+    });
+  });
 });
